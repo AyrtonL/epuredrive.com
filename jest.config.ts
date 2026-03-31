eaea8ab6e3fe7372de4404072a9cbad6b82b2084
Ayrton <route305.rental@gmail.com>
2026-03-30 21:14:10 -0400

    chore: exclude everything-claude-code dir from Jest test discovery

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/', '/everything-claude-code/'],
 }
 
 export default createJestConfig(config)
